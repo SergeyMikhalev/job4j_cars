a1ecc8d268c8e1748d4e7dd596dc8a646937b25a
SergeyMikhalev <tolltroll@yandex.ru>
2022-11-22 18:10:19 +0300

    table auto_post column name changed from post_text to description

--- db/scripts/002_ddl_create_table_auto_post.sql
+++ db/scripts/002_ddl_create_table_auto_post.sql
@@ -1,6 +1,6 @@
 create table auto_post(
     id serial primary key,
-    post_text text not null ,
+    description text not null ,
     created timestamp without time zone not null,
     auto_user_id int not null references auto_user(id)
 );
